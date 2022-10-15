c1e2b8ebd7dde9c03ea0c5d63e522ac5bee00a00
Tulio Avellar <tuliorafaelga@hotmail.com>
2022-10-15 18:57:07 -0300

    Added the HTM, CSS, JS and midia files

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach(tab => {
+    tab.addEventListener("click", function() {
+        if(tab.classList.contains("selected")) {
+            return;
+        }
+        tabSelection(tab);
+        infoSelection(tab);
+    });
+})
+function tabSelection(tab) {
+    const tabSelected = document.querySelector(".tab.selected");
+    tabSelected.classList.remove("selected");
+    tab.classList.add("selected");
+}
+function infoSelection(tab) {
+    const infoSelected = document.querySelector(".info.selected");
+    infoSelected.classList.remove("selected");
+    const idInfoTab = `info-${tab.id}`;
+    const infoShow = document.getElementById(idInfoTab);
+    infoShow.classList.add("selected");
+}
