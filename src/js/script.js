f90a4c9c12b478dca529487ff37713b1c46b48a4
Tulio Avellar <105487326+tulioavellar@users.noreply.github.com>
2025-08-28 21:02:23 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
+/*
+this function does the tab selection, by adding the class
+"selected" to the tab selected, and removing the
+same class from the other one 
+*/
 function tabSelection(tab) {
     const tabSelected = document.querySelector(".tab.selected");
     tabSelected.classList.remove("selected");
     tab.classList.add("selected");
 }
-
+/*
+this function does the content selection, by adding the class
+"selected" to the content selected, and removing the
+same class from the other one 
+*/
 function infoSelection(tab) {
     const infoSelected = document.querySelector(".info.selected");
     infoSelected.classList.remove("selected");
@@ -11,7 +20,11 @@ function infoSelection(tab) {
     const infoShow = document.getElementById(idInfoTab);
     infoShow.classList.add("selected");
 }
-
+/*
+this event is responsible for enabling all objects with the class ".tab"
+to execute the display of the classes "selected" from the other
+two functions
+*/
 const tabs = document.querySelectorAll(".tab");
 tabs.forEach(tab => {
     tab.addEventListener("click", function() {
